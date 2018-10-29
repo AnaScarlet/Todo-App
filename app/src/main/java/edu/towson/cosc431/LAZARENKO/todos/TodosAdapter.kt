3aa8ad746770506d38b40b7a770398570e72116a
Anastasiya Lazarenko <laz1anastasiya@gmail.com>
2018-10-29 13:56:21 -0400

    actually finished assignment 5

--- app/src/main/java/edu/towson/cosc431/LAZARENKO/todos/TodosAdapter.kt
+++ app/src/main/java/edu/towson/cosc431/LAZARENKO/todos/TodosAdapter.kt
@@ -5,12 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import kotlinx.android.synthetic.main.todo_view.view.*
-import android.R.attr.x
 import android.content.Context
-import android.view.Display
-import android.content.Context.WINDOW_SERVICE
 import android.graphics.Point
 import android.view.WindowManager
 
@@ -25,18 +21,23 @@ class TodosAdapter(val todosList: List<Todo>, val controller:IController): Recyc
 
         // Create viewHolder for this little view
         val viewHolder = TodosViewHolder(view)
+        viewHolder.itemView.isLongClickable = true
 
         // Do actions here
         viewHolder.itemView.completedState.setOnClickListener {
-            val position = viewHolder.adapterPosition
-            controller.toggleCompleted(position)
-            notifyItemChanged(position)
+            completeEventListener(viewHolder)
+        }
+
+        viewHolder.itemView.setOnClickListener {
+            completeEventListener(viewHolder)
         }
 
         viewHolder.itemView.delete_btn.setOnClickListener {
-            val position = viewHolder.adapterPosition
-            controller.deleteTodo(position)
-            notifyItemRemoved(position)
+            deleteEventListener(viewHolder)
+        }
+
+        viewHolder.itemView.setOnLongClickListener{
+            deleteEventListener(viewHolder)
         }
 
         return viewHolder
@@ -54,11 +55,26 @@ class TodosAdapter(val todosList: List<Todo>, val controller:IController): Recyc
         holder.itemView.title.text = thisTodo.title
         holder.itemView.due_date.text = thisTodo.dueDate
         holder.itemView.completedState.isChecked = thisTodo.isCompleted
-        holder.itemView.contents_short.text = thisTodo.contents
+        if (thisTodo.contents.length < 50)
+            holder.itemView.contents_short.text = thisTodo.contents
+        else
+            holder.itemView.contents_short.text = thisTodo.contents.substring(0, 50)
+    }
+
+    private fun deleteEventListener(viewHolder: TodosViewHolder): Boolean {
+        val position = viewHolder.adapterPosition
+        controller.deleteTodo(position)
+        notifyItemRemoved(position)
+        return true
+    }
+
+    private fun completeEventListener(viewHolder: TodosViewHolder) {
+        val position = viewHolder.adapterPosition
+        controller.toggleCompleted(position)
+        notifyItemChanged(position)
     }
 
     fun getScreenWidth(parent: ViewGroup): Int {
-
         val wm = parent.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val display = wm.defaultDisplay
         val size = Point()
